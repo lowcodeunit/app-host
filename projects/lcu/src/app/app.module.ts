1865b76ec242795fba84f21c81cec1eccc9fa21b
mcgear <michael.gearhardt@gmail.com>
2021-01-25 12:35:29 -0700

    Initialized

---
+++ projects/lcu/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FathymSharedModule, LCUServiceSettings } from '@lcu/common';
+import { environment } from '../environments/environment';
+import { AppHostModule } from '@lowcodeunit/app-host-common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FathymSharedModule,
+    AppHostModule.forRoot()
+  ],
+  providers: [
+    {
+      provide: LCUServiceSettings,
+      useValue: FathymSharedModule.DefaultServiceSettings(environment)
+    }
+  ],
+  exports: [AppHostModule]
+})
+export class AppModule {}
