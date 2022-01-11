a71fc133f78f9416985c309763738ce3c61d5cce
xchanin <shannonwbruns@gmail.com>
2022-01-10 20:08:48 -0700

    remove theme picker

--- projects/common/src/lib/controls/toolbar/toolbar.component.ts
+++ projects/common/src/lib/controls/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { ThemeBuilderService, ThemePickerModel, ThemeBuilderConstants } from '@lowcodeunit/lcu-theme-builder-common';
+// import { ThemeBuilderService, ThemePickerModel, ThemeBuilderConstants } from '@lowcodeunit/lcu-theme-builder-common';
 import {
   Component,
   EventEmitter,
@@ -35,8 +35,7 @@ export class ToolbarComponent implements OnInit {
 
   //  Constructors
   constructor(
-    protected injector: Injector, 
-    protected themeBuilderService: ThemeBuilderService) {
+    protected injector: Injector) {
     this.ActionClick = new EventEmitter();
   }
 
@@ -59,34 +58,34 @@ export class ToolbarComponent implements OnInit {
      * Setup array of themes
      */
     protected setupThemes(): void {
-      const themes: Array<ThemePickerModel> = [
-        new ThemePickerModel(
-          {
-            ID: 'Fathym Brand',
-            Primary: ThemeBuilderConstants.document.getPropertyValue('--initial-primary'),
-            Accent: ThemeBuilderConstants.document.getPropertyValue('--initial-accent'),
-            Warn: ThemeBuilderConstants.document.getPropertyValue('--initial-warn')
-          }
-        ),
-        new ThemePickerModel(
-          {
-            ID: 'Yellow', 
-            Primary: '#ffcc11',
-            Accent: '#06a5ff',
-            Warn: '#990000'
-          }
-        ),
-        new ThemePickerModel(
-          {
-            ID: 'Pink',
-            Primary: '#a83271',
-            Accent: '#6103ff',
-            Warn: '#b9f013'
-          }
-        )
-      ];
+      // const themes: Array<ThemePickerModel> = [
+      //   new ThemePickerModel(
+      //     {
+      //       ID: 'Fathym Brand',
+      //       Primary: ThemeBuilderConstants.document.getPropertyValue('--initial-primary'),
+      //       Accent: ThemeBuilderConstants.document.getPropertyValue('--initial-accent'),
+      //       Warn: ThemeBuilderConstants.document.getPropertyValue('--initial-warn')
+      //     }
+      //   ),
+      //   new ThemePickerModel(
+      //     {
+      //       ID: 'Yellow', 
+      //       Primary: '#ffcc11',
+      //       Accent: '#06a5ff',
+      //       Warn: '#990000'
+      //     }
+      //   ),
+      //   new ThemePickerModel(
+      //     {
+      //       ID: 'Pink',
+      //       Primary: '#a83271',
+      //       Accent: '#6103ff',
+      //       Warn: '#b9f013'
+      //     }
+      //   )
+      // ];
 
-      this.themeBuilderService.SetThemes(themes);
+      // this.themeBuilderService.SetThemes(themes);
     }
 
   protected buildMenuAction() {
